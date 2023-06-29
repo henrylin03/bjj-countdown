7998fc9eceff18eabc4220761edb2652e5c7847f
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-06-29 10:02:43 +1000

    refactor js script, change input to an actual button (not a submit) - ensured that countdown does not restart without clicking the 'start countdown' button first

--- js/script.js
+++ js/script.js
@@ -1,86 +1,73 @@
+// get elements from DOM
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+const competitionForm = document.getElementById("next-competition-form");
+const competitionDateInputElement = document.getElementById("competition-date");
+const competitionNameInputElement = document.getElementById(
+  "competition-name-input"
+);
+const competitionNameDisplay = document.getElementById(
+  "competition-name-display"
+);
+const submitButton = document.getElementById("submit-button");
+const errorMessage = document.getElementById("error-message");
+
+let countdownInterval;
+
+// function to add leading zeroes to single-digit numbers
 const addLeadingZeroes = (value) => (value < 10 ? `0${value}` : value);
 
-//helper function to check if the competition date entered by user is in the future
-const competitionDateInFuture = () => {
-  const competitionDateInput = document.getElementById("competition-date");
-  const competitionDate = new Date(competitionDateInput.value);
-  const now = new Date();
-
-  return competitionDate > now;
-};
-
-//prevents submission of form if value of competition date invalid
-const checkValuesAndEnableSubmission = () => {
-  const submitButton = document.getElementById("submit-button");
-  const errorMessage = document.getElementById("error-message");
-
-  submitButton.disabled = true;
-  if (!competitionDateInFuture()) {
-    errorMessage.innerHTML = "Please select a date in the future";
-    errorMessage.style.visibility = "visible";
-    return;
-  }
-  submitButton.disabled = false;
-  errorMessage.style.visibility = "hidden";
-};
-
-//calculates time until competition from now
-const setTimeUntilCompetition = () => {
-  let now = new Date().getTime();
-  const competitionDate = new Date(
-    document.getElementById("competition-date").value
-  );
-  const timeUntilCompetition = competitionDate - now;
-
-  //calculate each time unit from "now" until competition date
-  days = Math.floor(timeUntilCompetition / (1000 * 60 * 60 * 24));
-  hours = Math.floor(
-    (timeUntilCompetition % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  minutes = Math.floor((timeUntilCompetition % (1000 * 60 * 60)) / (1000 * 60));
-  seconds = Math.floor((timeUntilCompetition % (1000 * 60)) / 1000);
-
-  //update countdown elements
-  document.getElementById("days").innerHTML = addLeadingZeroes(days);
-  document.getElementById("hours").innerHTML = addLeadingZeroes(hours);
-  document.getElementById("minutes").innerHTML = addLeadingZeroes(minutes);
-  document.getElementById("seconds").innerHTML = addLeadingZeroes(seconds);
-};
-
-const titleCase = (str) => {
-  str = str
+//function to title case the competition name
+const titleCase = (str) =>
+  str
     .toLowerCase()
     .split(" ")
-    .map((word) => word.replace(word[0], word[0].toUpperCase()));
+    .map((word) => word.replace(word[0], word[0].toUpperCase()))
+    .join(" ");
 
-  return str.join(" ");
-};
+// event handler to update the #countdown-timer section
+const startCountdown = () => {
+  const compNameValue = competitionNameInputElement.value;
+  const compDateValue = competitionDateInputElement.value;
+  competitionNameDisplay.textContent = compNameValue;
 
-const showCompetitionName = () => {
-  const competitionNameInput = document.getElementById(
-    "competition-name-input"
-  );
-  const competitionNameInputted = competitionNameInput.value;
-  const competitionNameDisplay = document.getElementById("competition-name");
-
-  if (!competitionNameInputted) {
-    return;
+  // if countdown already occurring, clear (otherwise, the countdown timer will flash)
+  if (countdownInterval) {
+    clearInterval(countdownInterval);
   }
 
-  competitionNameDisplay.innerHTML = titleCase(competitionNameInputted);
+  // function to calculate and update time until competition
+  const findAndUpdateTimeUntilCompetition = () => {
+    const now = new Date().getTime();
+    const competitionDate = new Date(compDateValue).getTime();
+    const timeUntilCompetition = competitionDate - now;
+
+    if (timeUntilCompetition < 0) {
+      errorMessage.textContent = "Please select a date in the future";
+      return;
+    }
+
+    const days = Math.floor(timeUntilCompetition / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeUntilCompetition % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeUntilCompetition % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeUntilCompetition % (1000 * 60)) / 1000);
+
+    // display time
+    daysElement.textContent = addLeadingZeroes(days);
+    hoursElement.textContent = addLeadingZeroes(hours);
+    minutesElement.textContent = addLeadingZeroes(minutes);
+    secondsElement.textContent = addLeadingZeroes(seconds);
+  };
+
+  // repeatedly call function to update countdown every second (1000 ms)
+  countdownInterval = setInterval(findAndUpdateTimeUntilCompetition, 1000);
 };
 
-const setCountdown = (event) => {
-  event.preventDefault(); //prevents form submission
-  setInterval(setTimeUntilCompetition, 1000);
-  showCompetitionName();
-};
-
-//run script
-const competitionDateInput = document.getElementById("competition-date");
-competitionDateInput.addEventListener("blur", checkValuesAndEnableSubmission); //when user clicks off input
-
-const submitButton = document.getElementById("submit-button");
-submitButton.addEventListener("click", setCountdown);
-
-competitionDateInput.addEventListener("input", checkValuesAndEnableSubmission);
+// add event listener when submit button clicks
+submitButton.addEventListener("click", startCountdown);
