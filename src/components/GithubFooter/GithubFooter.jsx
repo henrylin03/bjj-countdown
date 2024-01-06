6f572a66b9028e82184222ed32e28ceabb3ce8a1
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-01-06 12:19:32 +1100

    use footer tag for github footer component for semantic html

--- src/components/GithubFooter/GithubFooter.jsx
+++ src/components/GithubFooter/GithubFooter.jsx
@@ -3,19 +3,21 @@ import { IconBrandGithub } from "@tabler/icons-react";
 
 function GithubFooter() {
   return (
-    <ActionIcon
-      size={50}
-      variant="filled"
-      color="#2e294e"
-      radius="xl"
-      aria-label="Link to my GitHub"
-      component="a"
-      href="https://github.com/henrylin03/"
-      target="_blank"
-      mb="xl"
-    >
-      <IconBrandGithub size={30} stroke={2.3} />
-    </ActionIcon>
+    <footer>
+      <ActionIcon
+        size={50}
+        variant="filled"
+        color="#2e294e"
+        radius="xl"
+        aria-label="Link to my GitHub"
+        component="a"
+        href="https://github.com/henrylin03/"
+        target="_blank"
+        mb="xl"
+      >
+        <IconBrandGithub size={30} stroke={2.3} />
+      </ActionIcon>
+    </footer>
   );
 }
 
